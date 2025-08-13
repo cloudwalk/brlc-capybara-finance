d89af5a5849271053e41cd8c75800011f33b6c52
Evgenii Zaitsev <evgenii@cloudwalk.io>
2025-08-13 15:36:30 +0700

    style: fix Prettier issues

--- contracts/libraries/Loan.sol
+++ contracts/libraries/Loan.sol
@@ -24,7 +24,7 @@ library Loan {
      * @dev A struct that defines the stored state of a loan.
      *
      * Fields:
-     * 
+     *
      * - programId -------------- The unique identifier of the program.
      * - borrowedAmount --------- The initial borrowed amount of the loan, excluding the addon.
      * - addonAmount ------------ The amount of the loan addon (extra charges or fees).
@@ -76,7 +76,7 @@ library Loan {
      * @dev A struct that defines the terms of a loan.
      *
      * Fields:
-     * 
+     *
      * - token ------------------ The address of the token to be used for the loan.
      * - addonAmount ------------ The amount of the loan addon (extra charges or fees).
      * - durationInPeriods ------ The total duration of the loan determined by the number of periods.
@@ -100,7 +100,7 @@ library Loan {
      * @dev A struct that defines the preview of the loan.
      *
      * Fields:
-     * 
+     *
      * - periodIndex ------------ The period index that matches the preview timestamp.
      * - trackedBalance --------- The tracked balance of the loan at the previewed period.
      * - outstandingBalance ----- The outstanding balance of the loan at the previewed period.
@@ -118,7 +118,7 @@ library Loan {
      * @dev A struct that defines the extended preview of a loan.
      *
      * Fields:
-     * 
+     *
      * - periodIndex ------------ The period index that matches the preview timestamp.
      * - trackedBalance --------- The tracked balance of the loan at the previewed period.
      * - outstandingBalance ----- The outstanding balance of the loan at the previewed period.
