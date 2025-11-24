7cc7e37464470af32298b9c40d691f597d8b02a7
Ilia Groshev <ilia.groshev@cloudwalk.io>
2025-11-24 17:04:38 +0000

    chore: deps and tools

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -43,7 +43,7 @@ const config: HardhatUserConfig = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: Number(process.env.OPTIMIZER_RUNS ?? 1000),
+        runs: Number(process.env.OPTIMIZER_RUNS ?? 200),
       },
     },
   },
